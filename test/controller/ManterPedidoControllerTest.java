517664b0b49e44a53e520a43d908f50c2d57f696
Caio Rezende <caiocezar50@outlook.com>
2020-04-25 17:36:37 -0300

    teste 'testConfirmarOperacao_inclusao' terminado

--- test/controller/ManterPedidoControllerTest.java
+++ test/controller/ManterPedidoControllerTest.java
@@ -5,8 +5,11 @@ import java.io.UnsupportedEncodingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.easymock.Capture;
+import static org.easymock.EasyMock.capture;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.newCapture;
 import static org.easymock.EasyMock.replay;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,12 +24,14 @@ public class ManterPedidoControllerTest {
     HttpServletRequest request;
     HttpServletResponse response;
     ManterPedidoController instance;
-
+    Capture<String> strArgument;
+    
     @Before
     public void setUp() {
         request = createMock(HttpServletRequest.class);
         response = createMock(HttpServletResponse.class);
         instance = new ManterPedidoController();
+        strArgument = newCapture();
     }
 
     @Test
@@ -54,21 +59,22 @@ public class ManterPedidoControllerTest {
     @Test
     public void testConfirmarOperacao_inclusao() throws UnsupportedEncodingException, IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        expect(request.getContextPath()).andReturn("htttp://teste.teste");
+        expect(request.getContextPath()).andReturn("");
+        response.sendRedirect(capture(strArgument));
         
         expect(request.getParameter("acao")).andReturn("confirmarOperacao");
         expect(request.getParameter("operacao")).andReturn("incluir");
         expect(request.getParameter("inputCodigo")).andReturn("");
-        expect(request.getParameter("hiddenProdutos")).andReturn("");
+        expect(request.getParameter("hiddenProdutos")).andReturn("1,2;");
         expect(request.getParameter("inputTipo")).andReturn("Interno");
         expect(request.getParameter("inputFuncionario")).andReturn("1");
         expect(request.getParameter("inputCliente")).andReturn("1");
+        expect(request.getParameter("inputEstado")).andReturn("Pendente");
         
         replay(request);
         replay(response);
         
         instance.processRequest(request, response);
-        
-        response.sendRedirect("htttp://teste.teste/pedidos");
+        assertEquals("/pedidos", strArgument.toString());
     }
 }
